baa813dc89e55206282f9460e901daf149c39f2b
Jurgen <jurgen.kluft@gmail.com>
2022-08-17 18:00:50 +0800

    * style change

--- package/package.go
+++ package/package.go
@@ -1,66 +1,66 @@
-package xbasepkgs
+package cbasepkgs
 
 import (
+	cactor "github.com/jurgen-kluft/cactor/package"
+	callocator "github.com/jurgen-kluft/callocator/package"
+	cbase "github.com/jurgen-kluft/cbase/package"
+	cbinmap "github.com/jurgen-kluft/cbinmap/package"
+	ccmdline "github.com/jurgen-kluft/ccmdline/package"
 	"github.com/jurgen-kluft/ccode/denv"
-	xactor "github.com/jurgen-kluft/xactor/package"
-	xallocator "github.com/jurgen-kluft/xallocator/package"
-	xbase "github.com/jurgen-kluft/xbase/package"
-	xbinmap "github.com/jurgen-kluft/xbinmap/package"
-	xcmdline "github.com/jurgen-kluft/xcmdline/package"
-	xcompress "github.com/jurgen-kluft/xcompress/package"
-	xcore "github.com/jurgen-kluft/xcore/package"
-	xcrypto "github.com/jurgen-kluft/xcrypto/package"
-	xecs "github.com/jurgen-kluft/xecs/package"
-	xentry "github.com/jurgen-kluft/xentry/package"
-	xfilesystem "github.com/jurgen-kluft/xfilesystem/package"
-	xgenerics "github.com/jurgen-kluft/xgenerics/package"
-	xhash "github.com/jurgen-kluft/xhash/package"
-	xlang "github.com/jurgen-kluft/xlang/package"
-	xp2p "github.com/jurgen-kluft/xp2p/package"
-	xrandom "github.com/jurgen-kluft/xrandom/package"
-	xsocket "github.com/jurgen-kluft/xsocket/package"
-	xstring "github.com/jurgen-kluft/xstring/package"
-	xsystem "github.com/jurgen-kluft/xsystem/package"
-	xtext "github.com/jurgen-kluft/xtext/package"
-	xthread "github.com/jurgen-kluft/xthread/package"
-	xtime "github.com/jurgen-kluft/xtime/package"
-	xunittest "github.com/jurgen-kluft/xunittest/package"
-	xuuid "github.com/jurgen-kluft/xuuid/package"
-	xvmem "github.com/jurgen-kluft/xvmem/package"
+	ccompress "github.com/jurgen-kluft/ccompress/package"
+	ccore "github.com/jurgen-kluft/ccore/package"
+	ccrypto "github.com/jurgen-kluft/ccrypto/package"
+	cecs "github.com/jurgen-kluft/cecs/package"
+	centry "github.com/jurgen-kluft/centry/package"
+	cfilesystem "github.com/jurgen-kluft/cfilesystem/package"
+	cgenerics "github.com/jurgen-kluft/cgenerics/package"
+	chash "github.com/jurgen-kluft/chash/package"
+	clang "github.com/jurgen-kluft/clang/package"
+	cp2p "github.com/jurgen-kluft/cp2p/package"
+	crandom "github.com/jurgen-kluft/crandom/package"
+	csocket "github.com/jurgen-kluft/csocket/package"
+	cstring "github.com/jurgen-kluft/cstring/package"
+	csystem "github.com/jurgen-kluft/csystem/package"
+	ctext "github.com/jurgen-kluft/ctext/package"
+	cthread "github.com/jurgen-kluft/cthread/package"
+	ctime "github.com/jurgen-kluft/ctime/package"
+	cunittest "github.com/jurgen-kluft/cunittest/package"
+	cuuid "github.com/jurgen-kluft/cuuid/package"
+	cvmem "github.com/jurgen-kluft/cvmem/package"
 )
 
-// GetPackage returns the package object of 'xbase'
+// GetPackage returns the package object of 'cbase'
 func GetPackage() *denv.Package {
 
 	// Dependencies
-	actorpkg := xactor.GetPackage()
-	allocatorpkg := xallocator.GetPackage()
-	basepkg := xbase.GetPackage()
-	binmappkg := xbinmap.GetPackage()
-	cmdlinepkg := xcmdline.GetPackage()
-	compresspkg := xcompress.GetPackage()
-	corepkg := xcore.GetPackage()
-	cryptopkg := xcrypto.GetPackage()
-	ecspkg := xecs.GetPackage()
-	entrypkg := xentry.GetPackage()
-	filesystempkg := xfilesystem.GetPackage()
-	genericspkg := xgenerics.GetPackage()
-	hashpkg := xhash.GetPackage()
-	langpkg := xlang.GetPackage()
-	p2ppkg := xp2p.GetPackage()
-	randompkg := xrandom.GetPackage()
-	socketpkg := xsocket.GetPackage()
-	stringpkg := xstring.GetPackage()
-	systempkg := xsystem.GetPackage()
-	textpkg := xtext.GetPackage()
-	threadpkg := xthread.GetPackage()
-	timepkg := xtime.GetPackage()
-	unittestpkg := xunittest.GetPackage()
-	uuidpkg := xuuid.GetPackage()
-	vmempkg := xvmem.GetPackage()
+	actorpkg := cactor.GetPackage()
+	allocatorpkg := callocator.GetPackage()
+	basepkg := cbase.GetPackage()
+	binmappkg := cbinmap.GetPackage()
+	cmdlinepkg := ccmdline.GetPackage()
+	compresspkg := ccompress.GetPackage()
+	corepkg := ccore.GetPackage()
+	cryptopkg := ccrypto.GetPackage()
+	ecspkg := cecs.GetPackage()
+	entrypkg := centry.GetPackage()
+	filesystempkg := cfilesystem.GetPackage()
+	genericspkg := cgenerics.GetPackage()
+	hashpkg := chash.GetPackage()
+	langpkg := clang.GetPackage()
+	p2ppkg := cp2p.GetPackage()
+	randompkg := crandom.GetPackage()
+	socketpkg := csocket.GetPackage()
+	stringpkg := cstring.GetPackage()
+	systempkg := csystem.GetPackage()
+	textpkg := ctext.GetPackage()
+	threadpkg := cthread.GetPackage()
+	timepkg := ctime.GetPackage()
+	unittestpkg := cunittest.GetPackage()
+	uuidpkg := cuuid.GetPackage()
+	vmempkg := cvmem.GetPackage()
 
-	// The main (xbase) package
-	mainpkg := denv.NewPackage("xbasepkgs")
+	// The main (cbasepkgs) package
+	mainpkg := denv.NewPackage("cbasepkgs")
 	mainpkg.AddPackage(actorpkg)
 	mainpkg.AddPackage(allocatorpkg)
 	mainpkg.AddPackage(basepkg)
@@ -87,8 +87,8 @@ func GetPackage() *denv.Package {
 	mainpkg.AddPackage(uuidpkg)
 	mainpkg.AddPackage(vmempkg)
 
-	// 'xbase' library
-	mainlib := denv.SetupDefaultCppLibProject("xbasepkgs", "github.com\\jurgen-kluft\\xbasepkgs")
+	// 'cbasepkgs' library
+	mainlib := denv.SetupDefaultCppLibProject("cbasepkgs", "github.com\\jurgen-kluft\\cbasepkgs")
 	mainlib.Dependencies = append(mainlib.Dependencies, unittestpkg.GetMainLib())
 
 	mainpkg.AddMainLib(mainlib)
